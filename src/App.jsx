5bf385bf5978093f83742b71849f30e3f2dd9e4b
Rizz-ke <wafiqmohameddoo@gmail.com>
2024-04-28 17:41:07 +0300

    bank-of-flatiron

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import '../src/App.jsx'
+
+function App() {
+  return {
+    <div className="segment raise">
+    <div className='text-header'>
+<h2>The Royal Bank of Flatiron</h2>
+    </div>
+    </div>
+  };
+}
+
+export default App;
